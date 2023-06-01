0cce64907c6df2fcea6c76c8f4f6177aab357f50
juancodev <montillasanchezjuancarlos@gmail.com>
2023-06-01 12:55:06 -0400

    [⚡ ADD] collistion with goal emoji

--- game.js
+++ game.js
@@ -3,41 +3,142 @@
  */
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.getElementById('up');
+const btnDown = document.getElementById('down');
+const btnLeft = document.getElementById('left');
+const btnRight = document.getElementById('right');
 
-window.addEventListener('load', startGame)
+window.addEventListener('load', startGame);
+window.addEventListener('resize', startGame);
+window.addEventListener('keydown', playerMove);
+btnUp.addEventListener('click', moveUp);
+btnDown.addEventListener('click', moveDown);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+}
+
+const giftPosition = {
+  x: undefined,
+  y: undefined,
+}
+
+let elementsSize;
+let canvasSize;
 
 function startGame() {
-
 
   let valueWidth = window.innerWidth;
   let valueHeight = window.innerHeight;
 
-  let canvasSize = (valueWidth + valueHeight) / 2 * 0.55;
+  canvasSize = Math.round((valueWidth + valueHeight) / 2 * 0.5);
 
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
 
-  let elementsSize = canvasSize / 10;
+  elementsSize = canvasSize / 10;
 
+  const mapRows = maps[0].trim().split('\n'); // clear string
+  const mapRowsCols = mapRows.map(cols => cols.trim().split(''));
   console.log({
-    canvasSize,
-    elementsSize
+    mapRows,
+    mapRowsCols
   });
 
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], elementsSize * i, elementsSize);
+  game.clearRect(0, 0, canvasSize, canvasSize);
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+
+      if (
+        mapRowsCols[row - 1][col - 1] == 'O' &&
+        playerPosition.x === undefined &&
+        playerPosition.y === undefined
+      ) {
+        playerPosition.x = Math.round(col * elementsSize);
+        playerPosition.y = Math.round(row * elementsSize);
+        console.log({
+          playerPosition
+        })
+      } else if (mapRowsCols[row - 1][col - 1] == 'I') {
+        giftPosition.x = Math.round(col * elementsSize);
+        giftPosition.y = Math.round(row * elementsSize);
+        console.log({
+          giftPosition
+        })
+      }
+
+
+      game.fillText(emojis[mapRowsCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+    }
   }
+  moveEmojiPlayer();
+
   // window.innerHeight()
   // window.innerWidth()
-  // game.fillRect(0, 0, 100, 100);
-  // game.clearRect(0, 0, 50, 50);
-  // game.clearRect(0, 0, 100, 50);
+};
 
-  // game.fillText('Test', 50, 50);
-  // game.fillStyle = 'indigo';
-  // game.textAlign = 'start';
-  // game.font = '25px Verdana';
+function moveEmojiPlayer() {
+  game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
+
+function playerMove(event) {
+  switch (event.code) {
+    case 'ArrowUp':
+      moveUp();
+      break;
+    case 'ArrowDown':
+      moveDown();
+      break;
+    case 'ArrowLeft':
+      moveLeft();
+      break;
+    case 'ArrowRight':
+      moveRight();
+      break;
+  }
+};
+
+function moveUp() {
+  if ((playerPosition.y - elementsSize) < elementsSize) {
+    console.log('OUT!');
+  } else {
+    playerPosition.y -= elementsSize;
+    startGame();
+    if (playerPosition.x == giftPosition.x && playerPosition.y == giftPosition.y) {
+      alert('congratulation');
+    }
+  }
+}
+
+function moveDown() {
+  if ((playerPosition.y + elementsSize) > canvasSize) {
+    console.log('OUT!');
+  } else {
+    playerPosition.y += elementsSize;
+    startGame();
+  }
+};
+
+function moveLeft() {
+  if ((playerPosition.x - elementsSize) < elementsSize) {
+    console.log('OUT!');
+  } else {
+    playerPosition.x -= elementsSize;
+    startGame();
+  }
+};
+
+function moveRight() {
+  if ((playerPosition.x + elementsSize) > canvasSize) {
+    console.log('OUT!');
+  } else {
+    playerPosition.x += elementsSize;
+    startGame();
+  }
+};
