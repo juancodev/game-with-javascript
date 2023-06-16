eb78497b77837d20456ab1bf198d3fb2ad39d9ba
juancodev <montillasanchezjuancarlos@gmail.com>
2023-06-16 17:37:09 -0400

    [⚡ADD] Project finish

--- game.js
+++ game.js
@@ -7,6 +7,13 @@ const btnUp = document.getElementById('up');
 const btnDown = document.getElementById('down');
 const btnLeft = document.getElementById('left');
 const btnRight = document.getElementById('right');
+const playerLives = document.getElementById('lives');
+const playerTimesNow = document.getElementById('time');
+const playerTimesRecord = document.getElementById('record');
+const playerFinished = document.getElementById('finish');
+const countTimeout = document.getElementById('timeout');
+const btnRestart = document.getElementById('restart');
+
 
 window.addEventListener('load', startGame);
 window.addEventListener('resize', startGame);
@@ -24,10 +31,18 @@ const playerPosition = {
 const giftPosition = {
   x: undefined,
   y: undefined,
-}
+};
+
+let enemyPositions = [];
 
 let elementsSize;
 let canvasSize;
+let level = 0;
+let lives = 3;
+let timeInterval;
+let timeStart;
+let timePlayer;
+let keyLocalStorage = 'playerTimeRecord'
 
 function startGame() {
 
@@ -41,17 +56,34 @@ function startGame() {
 
   elementsSize = canvasSize / 10;
 
-  const mapRows = maps[0].trim().split('\n'); // clear string
+  const map = maps[level];
+
+  if (!map) {
+    gameWinAndRecord();
+    return;
+  }
+
+  if (!timeStart) {
+    timeStart = Date.now();
+    timeInterval = setInterval(showTime, 100);
+    showRecord();
+  }
+
+  const mapRows = map.trim().split('\n'); // clear string
   const mapRowsCols = mapRows.map(cols => cols.trim().split(''));
-  console.log({
-    mapRows,
-    mapRowsCols
-  });
+
+  showLives();
 
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
 
+  // clear enemies and player position
+
+  enemyPositions = [];
   game.clearRect(0, 0, canvasSize, canvasSize);
+
+  // for each icon in the map.
+
   for (let row = 1; row <= 10; row++) {
     for (let col = 1; col <= 10; col++) {
 
@@ -62,15 +94,17 @@ function startGame() {
       ) {
         playerPosition.x = Math.round(col * elementsSize);
         playerPosition.y = Math.round(row * elementsSize);
-        console.log({
-          playerPosition
-        })
+
       } else if (mapRowsCols[row - 1][col - 1] == 'I') {
-        giftPosition.x = Math.round(col * elementsSize);
-        giftPosition.y = Math.round(row * elementsSize);
-        console.log({
-          giftPosition
-        })
+        giftPosition.x = Math.ceil(col * elementsSize);
+        giftPosition.y = Math.ceil(row * elementsSize);
+
+      } else if (mapRowsCols[row - 1][col - 1] == 'X') {
+        enemyPositions.push({
+          x: Math.round(col * elementsSize),
+          y: Math.round(row * elementsSize)
+        });
+
       }
 
 
@@ -85,7 +119,23 @@ function startGame() {
 
 function moveEmojiPlayer() {
   game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
+
+  const enemyCollision = enemyPositions.find(enemy => {
+    const enemyCollisionX = Math.round(enemy.x);
+    const enemyCollisionY = Math.round(enemy.y);
+    if (enemyCollisionX == playerPosition.x && enemyCollisionY == playerPosition.y) {
+      return enemyCollisionX && enemyCollisionY;
+    }
+  });
+
+  if (enemyCollision) {
+    game.fillText(emojis['BOMB_COLLISION'], playerPosition.x, playerPosition.y);
+    lives -= 1;
+    levelFail();
+  }
+
 }
+
 
 function playerMove(event) {
   switch (event.code) {
@@ -110,8 +160,8 @@ function moveUp() {
   } else {
     playerPosition.y -= elementsSize;
     startGame();
-    if (playerPosition.x == giftPosition.x && playerPosition.y == giftPosition.y) {
-      alert('congratulation');
+    if ((playerPosition.x == giftPosition.x) && (playerPosition.y == giftPosition.y)) {
+      levelUp();
     }
   }
 }
@@ -122,6 +172,9 @@ function moveDown() {
   } else {
     playerPosition.y += elementsSize;
     startGame();
+    if (playerPosition.x == giftPosition.x && playerPosition.y == giftPosition.y) {
+      levelUp();
+    }
   }
 };
 
@@ -131,6 +184,9 @@ function moveLeft() {
   } else {
     playerPosition.x -= elementsSize;
     startGame();
+    if (playerPosition.x == giftPosition.x && playerPosition.y == giftPosition.y) {
+      levelUp();
+    }
   }
 };
 
@@ -140,5 +196,85 @@ function moveRight() {
   } else {
     playerPosition.x += elementsSize;
     startGame();
+    if (playerPosition.x == giftPosition.x && playerPosition.y == giftPosition.y) {
+      levelUp();
+    }
   }
 };
+
+function levelUp() {
+  level++;
+  startGame();
+}
+
+function levelFail() {
+  if (lives <= 0) {
+    level = 0;
+    lives = 3;
+    timeStart = undefined;
+  }
+
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
+  startGame();
+}
+
+function gameWinAndRecord() {
+  clearInterval(timeInterval);
+
+  finish();
+
+  const totalTimeOfTheGame = playerTimesNow.getInnerHTML();
+  if (localStorage.length === 0) {
+    localStorage.setItem(keyLocalStorage, totalTimeOfTheGame);
+    playerTimesRecord.innerHTML = localStorage.getItem(keyLocalStorage);
+  } else if (localStorage.length !== 0 && localStorage.getItem(keyLocalStorage) > totalTimeOfTheGame) {
+    localStorage.removeItem(keyLocalStorage);
+    localStorage.setItem(keyLocalStorage, totalTimeOfTheGame);
+    playerTimesRecord.innerHTML = localStorage.getItem(keyLocalStorage);
+  } else {
+    playerTimesRecord.innerHTML = localStorage.getItem(keyLocalStorage);
+  }
+
+
+}
+
+function showLives() {
+  const heartsArray = Array(lives).fill(emojis['HEART']);
+
+  // clear for every moving step
+  playerLives.innerHTML = "";
+
+  for (let i = 0; i < heartsArray.length; i++) {
+    playerLives.append(heartsArray[i]);
+  }
+}
+
+function showTime() {
+  playerTimesNow.innerHTML = Date.now() - timeStart;
+}
+
+function showRecord() {
+  playerTimesRecord.innerHTML = localStorage.getItem(keyLocalStorage) || 0;
+}
+
+function finish() {
+  playerFinished.classList.remove('notVisible');
+  document.querySelector('.game-container').style.filter = "blur(5px)"
+
+  let count = 10;
+
+  let interval = setInterval(() => {
+    count--;
+    countTimeout.textContent = count.toString();
+
+    if (count === 0) {
+      clearInterval(interval);
+      location.reload();
+    }
+  }, 1000);
+
+  btnRestart.addEventListener('click', () => {
+    location.reload();
+  })
+}
